b4badbad92c4b4df5e6193cb03a622becddfb53f
Arin Gadre <54371368+807951@users.noreply.github.com>
2020-01-08 17:44:31 -0800

    1-8-2020

---
+++ HW 1-8-2020/Reverse.java
@@ -0,0 +1,71 @@
+/**
+ * makes reverseList methods
+ * @author Arin Gadre
+ * @version 1-8-2020
+ */
+import java.util.ArrayList;
+import java.util.Random;
+public class Reverse
+{
+    public Reverse(){
+        ArrayList<String> list1 = loadArrayList();
+        String[] list2 = loadArray();
+        ArrayList<String> list3 = reverseList(list1);
+        String[] list4 = reverseArr(list2);
+        
+        System.out.println(list1);
+        System.out.println(list3);
+        printArray(list2);
+        printArray(list4);
+    }
+
+    public ArrayList<String> reverseList(ArrayList<String>  list){
+        ArrayList<String> result = new ArrayList<String>();
+        for(int i = 0; i < list.size(); i++)
+            result.add(list.get(i));
+        return result;
+    }
+
+    public void printArray(String[] arr){
+        System.out.print("[");
+        for(int i = 0; i < arr.length-1; i++)
+            System.out.print(arr[i] + ", ");
+        System.out.print(arr[arr.length - 1] + "]");
+        System.out.println();
+    }
+
+    public String[] loadArray(){
+        String[] result = {randomStringGen(), randomStringGen(), randomStringGen(), randomStringGen(), randomStringGen(), randomStringGen(), randomStringGen(), randomStringGen(), randomStringGen(), randomStringGen()};
+        return result;
+    }
+
+    public ArrayList<String> loadArrayList(){
+        ArrayList<String> result = new ArrayList<String>();
+        for(int i = 0; i < 10; i++)
+            result.add(randomStringGen());
+        return result;
+    }
+
+    public String[] reverseArr(String[] list){
+        String[] result = new String[list.length];
+        for(int i = 0; i < list.length; i++)
+            result[i] = list[i];
+        return result;
+    }
+
+    public String randomStringGen(){
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+        int targetStringLength = random.nextInt(9) + 1;
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+            .limit(targetStringLength)
+            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+            .toString();
+        return generatedString;
+    }
+
+    public static void main(String[] args){
+        Reverse r = new Reverse();
+    }
+}
