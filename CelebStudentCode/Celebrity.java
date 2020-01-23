b6e8e2468153641e071aff0dbc15c6d25567a890
Arin Gadre <agadre@WS-ACADBUS.EDU>
2020-01-23 10:25:09 -0800

    1-23-2020

--- CelebStudentCode/Celebrity.java
+++ CelebStudentCode/Celebrity.java
@@ -5,66 +5,68 @@
  */
 public class Celebrity
 {
-	/**
-	 * The clue to determine the celebrity
-	 */
-	
-	/**
-	 * The answer or name of the celebrity.
-	 */
-	
-	/**
-	 * Creates a Celebrity instance with the supplied answer and clue
-	 * @param answer
-	 * @param clue
-	 */
-	public Celebrity(String answer, String clue)
-	{
-	}
+    /**
+     * The clue to determine the celebrity
+     */
+    private String clue;
+    /**
+     * The answer or name of the celebrity.
+     */
+    private String answer;
+    /**
+     * Creates a Celebrity instance with the supplied answer and clue
+     * @param answer
+     * @param clue
+     */
+    public Celebrity(String answer, String clue)
+    {
+        this.clue = clue;
+        this.answer = answer;
+    }
 
-	/**
-	 * Supplies the clue for the celebrity
-	 * @return
-	 */
-	public String getClue()
-	{
-		return null;
-	}
+    /**
+     * Supplies the clue for the celebrity
+     * @return
+     */
+    public String getClue()
+    {
+        return clue;
+    }
 
-	/**
-	 * Supplies the answer for the celebrity.
-	 * @return
-	 */
-	public String getAnswer()
-	{
-		return null;
-	}
+    /**
+     * Supplies the answer for the celebrity.
+     * @return
+     */
+    public String getAnswer()
+    {
+        return answer;
+    }
 
-	/**
-	 * Updates the clue to the provided String.
-	 * @param clue The new clue.
-	 */
-	public void setClue(String clue)
-	{
-		
-	}
+    /**
+     * Updates the clue to the provided String.
+     * @param clue The new clue.
+     */
+    public void setClue(String clue)
+    {
+        this.clue = clue;
+    }
 
-	/**
-	 * Updates the answer to the provided String.
-	 * @param answer The new answer.
-	 */
-	public void setAnswer(String answer)
-	{
-		
-	}
-	
-	/**
-	 * Provides a String representation of the Celebrity.
-	 */
-	@Override
-	public String toString()
-	{
-		return null;
-	}
-	
+    /**
+     * Updates the answer to the provided String.
+     * @param answer The new answer.
+     */
+    public void setAnswer(String answer)
+    {
+        this.answer = answer;
+    }
+
+    /**
+     * Provides a String representation of the Celebrity.
+     */
+    @Override
+    public String toString()
+    {
+        return "This celebrity's name is " + getAnswer() + " and their clue is " + getClue();
+    }
+
 }
--- CelebStudentCode/CelebrityGame.java
+++ CelebStudentCode/CelebrityGame.java
@@ -8,121 +8,127 @@ import java.util.ArrayList;
  */
 public class CelebrityGame
 {
-	/**
-	 * A reference to a Celebrity or subclass instance.
-	 */
+    /**
+     * A reference to a Celebrity or subclass instance.
+     */
+    Celebrity gameCelebrity;
+    /**
+     * The GUI frame for the Celebrity game.
+     */
+    private CelebrityFrame gameWindow;
+    /**
+     * The ArrayList of Celebrity values that make up the game
+     */
+    private ArrayList<Celebrity> celebGameList;
+    /**
+     * Builds the game and starts the GUI
+     */
+    public CelebrityGame()
+    {
+        celebGameList = new ArrayList<Celebrity>();
+        gameWindow = new CelebrityFrame(this);
+    }
 
-	/**
-	 * The GUI frame for the Celebrity game.
-	 */
+    /**
+     * Prepares the game to start by re-initializing the celebGameList and having the gameFrame open the start screen.
+     */
+    public void prepareGame()
+    {
+        celebGameList = new ArrayList<Celebrity>();
+        gameWindow.replaceScreen("START");
+    }
 
-	/**
-	 * The ArrayList of Celebrity values that make up the game
-	 */
+    /**
+     * Determines if the supplied guess is correct.
+     * 
+     * @param guess
+     *            The supplied String
+     * @return Whether it matches regardless of case or extraneous external
+     *         spaces.
+     */
+    public boolean processGuess(String guess)
+    {
+        return false;
+    }
 
-	/**
-	 * Builds the game and starts the GUI
-	 */
-	public CelebrityGame()
-	{
-	}
+    /**
+     * Asserts that the list is initialized and contains at least one Celebrity.
+     * Sets the current celebrity as the first item in the list. Opens the game
+     * play screen.
+     */
+    public void play()
+    {
 
-	/**
-	 * Prepares the game to start by re-initializing the celebGameList and having the gameFrame open the start screen.
-	 */
-	public void prepareGame()
-	{
-	}
+    }
 
-	/**
-	 * Determines if the supplied guess is correct.
-	 * 
-	 * @param guess
-	 *            The supplied String
-	 * @return Whether it matches regardless of case or extraneous external
-	 *         spaces.
-	 */
-	public boolean processGuess(String guess)
-	{
-		return false;
-	}
+    /**
+     * Adds a Celebrity of specified type to the game list
+     * 
+     * @param name
+     *            The name of the celebrity
+     * @param guess
+     *            The clue(s) for the celebrity
+     * @param type
+     *            What type of celebrity
+     */
+    public void addCelebrity(String name, String guess, String type)
+    {
 
-	/**
-	 * Asserts that the list is initialized and contains at least one Celebrity.
-	 * Sets the current celebrity as the first item in the list. Opens the game
-	 * play screen.
-	 */
-	public void play()
-	{
-		
-	}
+    }
 
-	/**
-	 * Adds a Celebrity of specified type to the game list
-	 * 
-	 * @param name
-	 *            The name of the celebrity
-	 * @param guess
-	 *            The clue(s) for the celebrity
-	 * @param type
-	 *            What type of celebrity
-	 */
-	public void addCelebrity(String name, String guess, String type)
-	{
-		
-	}
+    /**
+     * Validates the name of the celebrity. It must have at least 4 characters.
+     * @param name The name of the Celebrity
+     * @return If the supplied Celebrity is valid
+     */
+    public boolean validateCelebrity(String name)
+    {
+        if(name.trim().length() >= 4)
+            return true;
+        return false;
+    }
 
-	/**
-	 * Validates the name of the celebrity. It must have at least 4 characters.
-	 * @param name The name of the Celebrity
-	 * @return If the supplied Celebrity is valid
-	 */
-	public boolean validateCelebrity(String name)
-	{
-		return false;
-	}
+    /**
+     * Checks that the supplied clue has at least 10 characters or is a series of clues
+     * This method would be expanded based on your subclass of Celebrity.
+     * @param clue The text of the clue(s)
+     * @param type Supports a subclass of Celebrity 
+     * @return If the clue is valid.
+     */
+    public boolean validateClue(String clue, String type)
+    {
+       return false;
+    }
 
-	/**
-	 * Checks that the supplied clue has at least 10 characters or is a series of clues
-	 * This method would be expanded based on your subclass of Celebrity.
-	 * @param clue The text of the clue(s)
-	 * @param type Supports a subclass of Celebrity 
-	 * @return If the clue is valid.
-	 */
-	public boolean validateClue(String clue, String type)
-	{
-		return false;
-	}
+    /**
+     * Accessor method for the current size of the list of celebrities
+     * 
+     * @return Remaining number of celebrities
+     */
+    public int getCelebrityGameSize()
+    {
+        return 0;
+    }
 
-	/**
-	 * Accessor method for the current size of the list of celebrities
-	 * 
-	 * @return Remaining number of celebrities
-	 */
-	public int getCelebrityGameSize()
-	{
-		return 0;
-	}
+    /**
+     * Accessor method for the games clue to maintain low coupling between
+     * classes
+     * 
+     * @return The String clue from the current celebrity.
+     */
+    public String sendClue()
+    {
+        return null;
+    }
 
-	/**
-	 * Accessor method for the games clue to maintain low coupling between
-	 * classes
-	 * 
-	 * @return The String clue from the current celebrity.
-	 */
-	public String sendClue()
-	{
-		return null;
-	}
-
-	/**
-	 * Accessor method for the games answer to maintain low coupling between
-	 * classes
-	 * 
-	 * @return The String answer from the current celebrity.
-	 */
-	public String sendAnswer()
-	{
-		return null;
-	}
+    /**
+     * Accessor method for the games answer to maintain low coupling between
+     * classes
+     * 
+     * @return The String answer from the current celebrity.
+     */
+    public String sendAnswer()
+    {
+        return null;
+    }
 }
